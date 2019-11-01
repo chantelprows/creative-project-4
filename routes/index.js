a828cd2bcb45b7c8224bb2310bce8998f1b62690
EC2 Default User <ec2-user@ip-172-31-28-7.us-west-2.compute.internal>
2019-11-01 22:10:32 +0000

    fixes

--- routes/index.js
+++ routes/index.js
@@ -8,29 +8,6 @@ router.get('/', function(req, res, next) {
     res.sendFile('index.html', { root: 'public' });
 });
 
-router.get('/getcity', function(req, res, next) {
-    var myRe = new RegExp("^" + req.query.q);
-
-    fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
-        if (err) throw err;
-        var cities = data.toString().split("\n");
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                console.log(cities[i]);
-            }
-        }
-        var jsonresult = [];
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                jsonresult.push({ city: cities[i] });
-            }
-        }
-        res.status(200).json(jsonresult);
-    });
-
-});
 
 router.get('/getfact', function(req, res, next) {
     var url = "https://cat-fact.herokuapp.com/facts"
